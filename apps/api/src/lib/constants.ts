30ad2efc5d311249802368e4612021eaefd76c52
AbdallahMostafaIbrahim <abdallahmostafaibrahim@gmail.com>
2023-08-22 17:22:28 +0200

    indentation

--- apps/api/src/lib/constants.ts
+++ apps/api/src/lib/constants.ts
@@ -7,9 +7,11 @@ export const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 export const PORT = process.env.PORT;
 
-export const MONGOURL = process.env.MONGOURL || `mongodb://${
-  DB_USER ? `${DB_USER}:${DB_PASS}@` : ``
-}${DB_HOST}:${DB_PORT}/${DB_NAME}${DB_USER ? `?authSource=admin` : ``}`;
+export const MONGOURL =
+  process.env.MONGOURL ||
+  `mongodb://${
+    DB_USER ? `${DB_USER}:${DB_PASS}@` : ``
+  }${DB_HOST}:${DB_PORT}/${DB_NAME}${DB_USER ? `?authSource=admin` : ``}`;
 
 const COOKIE_SECRET = process.env.COOKIE_SECRET!;
 export const production =
