e9ed72789820a269e63ef3092625c715f313041f
AbdallahMostafaIbrahim <abdallahmostafaibrahim@gmail.com>
2023-08-22 17:21:22 +0200

    option to use MONGOURL env

--- apps/api/src/lib/constants.ts
+++ apps/api/src/lib/constants.ts
@@ -7,7 +7,7 @@ export const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 export const PORT = process.env.PORT;
 
-export const MONGOURL = `mongodb://${
+export const MONGOURL = process.env.MONGOURL || `mongodb://${
   DB_USER ? `${DB_USER}:${DB_PASS}@` : ``
 }${DB_HOST}:${DB_PORT}/${DB_NAME}${DB_USER ? `?authSource=admin` : ``}`;
 
